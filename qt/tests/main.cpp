265e9d13173acfc167414e2822e13ba7d6399cce
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-08-29 21:19:16 +0200

    Create main.cpp

---
+++ qt/tests/main.cpp
@@ -0,0 +1,31 @@
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+
+// A test program to see if Qt works..
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QWidget window;
+    window.setWindowTitle("COSP-C");
+    QVBoxLayout *layout = new QVBoxLayout();
+
+    QLabel *label = new QLabel("COSP-C CydraOSPrograms CODE");
+    layout->addWidget(label);
+
+
+    QPushButton *button1 = new QPushButton("COSP-C");
+    label->setAlignment(Qt::AlignCenter);
+
+    layout->addStretch();
+    layout->addWidget(button1);
+    layout->addStretch();
+
+    window.setLayout(layout);
+    window.resize(320, 240);
+    window.show();
+
+    return app.exec();
+}
