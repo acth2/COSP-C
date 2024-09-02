6300b8b92fe9b4019b4d9423481c7cbeb5ae7657
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-02 19:25:15 +0200

    Update main.cpp

--- qt/windowmanager/main.cpp
+++ qt/windowmanager/main.cpp
@@ -7,21 +7,10 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    // Vérifiez si la variable d'environnement DISPLAY est définie en utilisant QProcess
-    QProcess process;
-    process.start("sh", QStringList() << "-c" << "echo $DISPLAY");
-    process.waitForFinished();
-    QByteArray output = process.readAllStandardOutput().trimmed();
+    WindowManager manager;
+    manager.setGeometry(0, 0, 800, 600);
+    manager.setWindowTitle("My Window Manager");
+    manager.show();
 
-    if (!output.isEmpty()) {
-        WindowManager manager;
-        manager.setGeometry(0, 0, 800, 600);
-        manager.setWindowTitle("My Window Manager");
-        manager.show();
-
-        return app.exec();
-    } else {
-        qDebug() << "Erreur : La variable d'environnement DISPLAY n'est pas définie. Assurez-vous d'avoir lancé un serveur X";
-        return 1;
-    }
+    return app.exec();
 }
