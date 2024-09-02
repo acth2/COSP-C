54bbb94e47ce040897b74603157725d722c4cbed
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-02 19:13:35 +0200

    Update main.cpp

--- qt/windowmanager/main.cpp
+++ qt/windowmanager/main.cpp
@@ -1,13 +1,20 @@
 #include <QApplication>
+#include <QMessageBox>
+#include <QX11Info>
 #include "windowmanager.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    
+    if (QX11Info::isPlatformX11()) {
+        WindowManager manager;
+        manager.setGeometry(0, 0, 800, 600);  
+        manager.setWindowTitle("My Window Manager");
+        manager.show();
 
-    WindowManager manager;
-    manager.setGeometry(0, 0, 800, 600);
-    manager.setWindowTitle("My Window Manager");
-    manager.show();
-
-    return app.exec();
+        return app.exec();
+    } else {
+         std::cerr << "Erreur : Aucun serveur X ne sont détectés... Veuillez lancer dans un environnement graphique." << std::endl;
+        return 1;
+    }
 }
