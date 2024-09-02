fd28ac33345516360ffa33bd63f0546ed41b01dc
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-02 19:36:15 +0200

    Update windowmanager.cpp

--- qt/windowmanager/windowmanager.cpp
+++ qt/windowmanager/windowmanager.cpp
@@ -3,6 +3,8 @@
 #include <QDesktopWidget>
 #include <QScreen>
 #include <QDebug>
+#include <QKeyEvent>
+#include <QProcess>
 
 WindowManager::WindowManager(QWidget *parent) : QWidget(parent) {
     QScreen *screen = QApplication::primaryScreen();
@@ -12,7 +14,6 @@ WindowManager::WindowManager(QWidget *parent) : QWidget(parent) {
     } else {
         qDebug() << "Erreur : Impossible d'obtenir les informations sur l'écran.";
     }
-
     setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
 }
 
@@ -20,5 +21,13 @@ bool WindowManager::event(QEvent *event) {
     if (event->type() == QEvent::WindowActivate) {
         qDebug() << "Window activated";
     }
-    return QWidget::event(event);
+    return QWidget::event(event); 
+}
+
+void WindowManager::keyPressEvent(QKeyEvent *event) {
+    if (event->key() == Qt::Key_P) {
+        qDebug() << "Touche 'p' pressée : fermeture du serveur X";
+        QProcess::execute("pkill Xorg");
+    }
+    QWidget::keyPressEvent(event);
 }
