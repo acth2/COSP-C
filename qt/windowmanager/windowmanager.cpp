4dfb831ed6e1d5a589949ced760fa197b571a478
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-02 20:20:44 +0200

    Update windowmanager.cpp

--- qt/windowmanager/windowmanager.cpp
+++ qt/windowmanager/windowmanager.cpp
@@ -5,6 +5,8 @@
 #include <QKeyEvent>
 #include <QProcess>
 #include <QCloseEvent>
+#include <QPainter>
+#include <QPixmap>
 
 WindowManager::WindowManager(QWidget *parent) : QWidget(parent) {
     setWindowFlags(Qt::Window | Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
@@ -30,13 +32,24 @@ bool WindowManager::event(QEvent *event) {
 void WindowManager::keyPressEvent(QKeyEvent *event) {
     qDebug() << "Touche pressée : " << event->key();
     if (event->key() == Qt::Key_P) {
-        qDebug() << "Touche 'p' pressée : Ouverture de TWM";
-        QProcess::execute("startx /usr/bin/twm");
+        qDebug() << "Touche 'p' pressée : fermeture du serveur X";
+        QProcess::execute("pkill Xorg");
     } else {
         QWidget::keyPressEvent(event);
     }
 }
 
 void WindowManager::closeEvent(QCloseEvent *event) {
+    qDebug() << "Tentative de fermeture ignorée";
     event->ignore();
+}
+
+void WindowManager::paintEvent(QPaintEvent *event) {
+    QPainter painter(this);
+    QPixmap backgroundPixmap("/usr/cydra/backgrounds/current.png");
+    if (!backgroundPixmap.isNull()) {
+        painter.drawPixmap(0, 0, width(), height(), backgroundPixmap);
+    } else {
+        qDebug() << "Erreur : Impossible de charger l'image de fond.";
+    }
 }
