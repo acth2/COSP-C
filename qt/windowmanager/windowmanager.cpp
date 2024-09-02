bb196c33dfe18de9ef552fab3099019bfb5e041b
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-02 19:08:16 +0200

    Update windowmanager.cpp

--- qt/windowmanager/windowmanager.cpp
+++ qt/windowmanager/windowmanager.cpp
@@ -3,16 +3,15 @@
 
 WindowManager::WindowManager(QWidget *parent)
     : QWidget(parent) {
-      
 }
 
-//WindowManager::~WindowManager() {
-//    
-//}
+WindowManager::~WindowManager() {
+
+}
 
 bool WindowManager::event(QEvent *event) {
     if (event->type() == QEvent::WindowActivate) {
         qDebug("Window activated");
     }
-    return QWidget::event(event);
+    return QWidget::event(event); 
 }
