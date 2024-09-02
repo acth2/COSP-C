23374abc026d753e465b7cdcdec90db895d7cb55
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-03 00:07:07 +0200

    Update konami_code_handler.h

--- qt/windowmanager/konami_code_handler.h
+++ qt/windowmanager/konami_code_handler.h
@@ -3,25 +3,24 @@
 
 #include <QObject>
 #include <QKeyEvent>
-#include <QProcess>
+
+class QLabel;
 
 class KonamiCodeHandler : public QObject {
     Q_OBJECT
 
 public:
-    explicit KonamiCodeHandler(QObject *parent = nullptr);
+    explicit KonamiCodeHandler(QLabel *logLabel, QObject *parent = nullptr);
 
     void keyPressEvent(QKeyEvent *event);
 
 private:
-    void openConsole();
-    void closeConsole();
+    void appendLog(const QString &message);
 
     static const int konamiCode[];
     static const int konamiCodeLength;
     int currentCodeIndex;
-    bool consoleOpened;
-    QProcess *consoleProcess;
+    QLabel *logLabel;
 };
 
 #endif // KONAMI_CODE_HANDLER_H
