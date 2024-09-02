782b0417e773bef3480664db2ba487729eb741c0
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-02 19:34:01 +0200

    Update windowmanager.cpp

--- qt/windowmanager/windowmanager.cpp
+++ qt/windowmanager/windowmanager.cpp
@@ -1,17 +1,24 @@
 #include "windowmanager.h"
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QScreen>
 #include <QDebug>
 
-WindowManager::WindowManager(QWidget *parent)
-    : QWidget(parent) {
-}
+WindowManager::WindowManager(QWidget *parent) : QWidget(parent) {
+    QScreen *screen = QApplication::primaryScreen();
+    if (screen) {
+        QRect screenGeometry = screen->geometry();
+        setGeometry(screenGeometry);
+    } else {
+        qDebug() << "Erreur : Impossible d'obtenir les informations sur l'écran.";
+    }
 
-WindowManager::~WindowManager() {
-
+    setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
 }
 
 bool WindowManager::event(QEvent *event) {
     if (event->type() == QEvent::WindowActivate) {
-        qDebug("Window activated");
+        qDebug() << "Window activated";
     }
-    return QWidget::event(event); 
+    return QWidget::event(event);
 }
