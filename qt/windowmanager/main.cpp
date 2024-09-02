368ff13b6f94c1f4d9a014ba768a35aed00bb6fa
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-02 23:40:29 +0200

    Update main.cpp

--- qt/windowmanager/main.cpp
+++ qt/windowmanager/main.cpp
@@ -11,9 +11,6 @@ int main(int argc, char *argv[]) {
     if (screen) {
         QRect screenGeometry = screen->geometry();
         manager.setGeometry(screenGeometry);
-        qDebug() << "Screen size:" << screenGeometry.size();
-    } else {
-        qDebug() << "Erreur : Impossible d'obtenir les informations sur l'écran.";
     }
 
     manager.setWindowTitle("CWM");
