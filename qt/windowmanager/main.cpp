8babe2b90de37f01992f063d82dabb4f91706cd2
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-02 19:21:17 +0200

    Update main.cpp

--- qt/windowmanager/main.cpp
+++ qt/windowmanager/main.cpp
@@ -1,21 +1,27 @@
 #include <QApplication>
-#include <QMessageBox>
-#include <cstdlib>
+#include <QDebug>
+#include <QProcess>
+#include <QByteArray>
 #include "windowmanager.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    
-    const char* display = std::getenv("DISPLAY");
-    if (display && *display != '\0') {
+
+    // Vérifiez si la variable d'environnement DISPLAY est définie en utilisant QProcess
+    QProcess process;
+    process.start("sh", QStringList() << "-c" << "echo $DISPLAY");
+    process.waitForFinished();
+    QByteArray output = process.readAllStandardOutput().trimmed();
+
+    if (!output.isEmpty()) {
         WindowManager manager;
-        manager.setGeometry(0, 0, 800, 600);  
+        manager.setGeometry(0, 0, 800, 600);
         manager.setWindowTitle("My Window Manager");
         manager.show();
 
         return app.exec();
     } else {
-        std::cerr << "Erreur : Aucun serveur X ne sont détectés... Veuillez lancer dans un environnement graphique." << std::endl;
+        qDebug() << "Erreur : La variable d'environnement DISPLAY n'est pas définie. Assurez-vous d'avoir lancé un serveur X";
         return 1;
     }
 }
