9bd58ab36c3a727a1ed9a6749b7dd5209b447445
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-03 19:46:10 +0200

    Update userinteractright.h

--- qt/windowmanager/userinteractright.h
+++ qt/windowmanager/userinteractright.h
@@ -1,73 +1,31 @@
-#include "userinteractright.h"
+#ifndef USERINTERACTRIGHT_H
+#define USERINTERACTRIGHT_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
 #include <QMouseEvent>
-#include <QPainter>
-#include <QPaintEvent>
-#include <QApplication>
 
-UserInteractRight::UserInteractRight(QWidget *parent) : QWidget(parent) {
-    setWindowFlags(Qt::Popup | Qt::FramelessWindowHint);
-    setAttribute(Qt::WA_TranslucentBackground);
+class UserInteractRight : public QWidget {
+    Q_OBJECT
 
-    button1 = new QPushButton("Exit system", this);
-    button2 = new QPushButton("Button 2", this);
-    button3 = new QPushButton("Button 3", this);
-    textLabel = new QLabel("This is a text label", this);
-    
-    button1->setStyleSheet("QPushButton { background-color: lightgray; border: none; }");
-    button2->setStyleSheet("QPushButton { background-color: lightgray; border: none; }");
-    button3->setStyleSheet("QPushButton { background-color: lightgray; border: none; }");
-    
-    textLabel->setStyleSheet("QLabel { background-color: transparent; }");
-    
-    QVBoxLayout *layout = new QVBoxLayout(this);
-    layout->addWidget(textLabel);
-    layout->addWidget(button1);
-    layout->addWidget(button2);
-    layout->addWidget(button3);
-    layout->setContentsMargins(10, 10, 10, 10);
-    setLayout(layout);
+public:
+    explicit UserInteractRight(QWidget *parent = nullptr);
 
-    connect(button1, &QPushButton::clicked, this, &UserInteractRight::button1Clicked);
-    
-    setFixedSize(200, 200);
-}
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
 
-void UserInteractRight::mousePressEvent(QMouseEvent *event) {
-    if (event->button() == Qt::RightButton) {
-        isMousePressed = true;
-        QPoint pos = event->globalPos();
-        move(pos.x() - 5, pos.y() + 10);
-        show();
-    }
-    QWidget::mousePressEvent(event);
-}
+private:
+    QPushButton *button1;
+    QPushButton *button2;
+    QPushButton *button3;
+    QLabel *textLabel;
+    bool isMousePressed = false;
 
-void UserInteractRight::mouseReleaseEvent(QMouseEvent *event) {
-    if (event->button() == Qt::RightButton) {
-        isMousePressed = false;
-        QPoint pos = event->globalPos();
-        if (!rect().contains(mapFromGlobal(pos))) {
-            close();
-        }
-    }
-    QWidget::mouseReleaseEvent(event);
-}
+    void checkOutsideClick(const QPoint &pos);
+    void button1Clicked();
+};
 
-void UserInteractRight::paintEvent(QPaintEvent *event) {
-    QPainter painter(this);
-    painter.setPen(Qt::gray);
-    painter.setBrush(Qt::lightGray);
-    painter.drawRect(rect());
-
-    QWidget::paintEvent(event);
-}
-
-void UserInteractRight::checkOutsideClick(const QPoint &pos) {
-    if (!rect().contains(mapFromGlobal(pos))) {
-        close();
-    }
-}
-
-void UserInteractRight::button1Clicked() {
-    QApplication::quit();
-}
+#endif // USERINTERACTRIGHT_H
