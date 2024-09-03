ed888749f74290e310400e71be1a1a9d85a5b13c
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-03 18:04:00 +0200

    Update konami_code_handler.cpp

--- qt/windowmanager/konami_code_handler.cpp
+++ qt/windowmanager/konami_code_handler.cpp
@@ -1,16 +1,30 @@
 #include "konami_code_handler.h"
 
-KonamiCodeHandler::KonamiCodeHandler(QObject *parent) : QObject(parent), currentSequence("") {}
+KonamiCodeHandler::KonamiCodeHandler(QObject *parent)
+    : QObject(parent), resetTimer(new QTimer(this)) {
+    
+    resetTimer->setInterval(2000);
+    resetTimer->setSingleShot(true);
+
+    connect(resetTimer, &QTimer::timeout, this, &KonamiCodeHandler::resetSequence);
+}
 
 void KonamiCodeHandler::handleKeyPress(QKeyEvent *event) {
-    currentSequence += QString::number(event->key());
+    resetTimer->start();
 
-    if (currentSequence.length() > konamiCode.length()) {
-        currentSequence = currentSequence.right(konamiCode.length());
-    }
+    if (!konamiCode.isEmpty() && event->key() == konamiCode[currentSequence.size()]) {
+        currentSequence.append(event->key());
 
-    if (currentSequence == konamiCode) {
-        emit konamiCodeEntered();
-        currentSequence.clear();
+        if (currentSequence == konamiCode) {
+            emit konamiCodeEntered();
+            resetSequence();
+        }
+    } else {
+        resetSequence();
     }
+}
+
+void KonamiCodeHandler::resetSequence() {
+    currentSequence.clear();
+    resetTimer->stop();
 }
