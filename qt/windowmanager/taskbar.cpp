17da6eea8b1b790fcef7cdc030490d96651863b5
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-04 16:35:01 +0200

    Update taskbar.cpp

--- qt/windowmanager/taskbar.cpp
+++ qt/windowmanager/taskbar.cpp
@@ -4,6 +4,10 @@
 #include <QPropertyAnimation>
 #include <QMouseEvent>
 #include <QKeyEvent>
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QLabel>
 
 TaskBar::TaskBar(QWidget *parent) : QWidget(parent) {
     setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
@@ -14,17 +18,24 @@ TaskBar::TaskBar(QWidget *parent) : QWidget(parent) {
     startButton->setIconSize(QSize(32, 32));
     startButton->setStyleSheet("border: none;");
 
+    powerButton = new QPushButton(this);
+    powerButton->setIcon(QIcon("/usr/cydra/icons/power.png"));
+    powerButton->setIconSize(QSize(32, 32));
+    powerButton->setStyleSheet("border: none;");
+
     QHBoxLayout *layout = new QHBoxLayout(this);
     layout->addWidget(startButton, 0, Qt::AlignLeft | Qt::AlignBottom);
+    layout->addWidget(powerButton, 0, Qt::AlignRight | Qt::AlignBottom);
     layout->setContentsMargins(5, 5, 5, 5);
     setLayout(layout);
 
     popup = new QLabel(nullptr);
     popup->setFixedSize(500, 500);
-    popup->setStyleSheet("background-color: #333333; border: 1px solid gray;"); 
+    popup->setStyleSheet("background-color: #333333; border: 1px solid gray;");
     popup->hide();
 
     connect(startButton, &QPushButton::clicked, this, &TaskBar::showPopup);
+    connect(powerButton, &QPushButton::clicked, this, &TaskBar::showPowerMenu);
 
     adjustSizeToScreen();
     installEventFilter();
@@ -83,4 +94,37 @@ bool TaskBar::eventFilter(QObject *object, QEvent *event) {
         }
     }
     return QWidget::eventFilter(object, event);
+}
+
+void TaskBar::showPowerMenu() {
+    QWidget *overlay = new QWidget(this);
+    overlay->setStyleSheet("background: rgba(0, 0, 0, 0.7);");
+    overlay->setGeometry(QApplication::primaryScreen()->geometry());
+    overlay->show();
+
+    QDialog *powerDialog = new QDialog(this);
+    powerDialog->setWindowTitle("Power Options");
+    powerDialog->setModal(true);
+    QVBoxLayout *dialogLayout = new QVBoxLayout(powerDialog);
+
+    QLabel *infoLabel = new QLabel("Do you want to reboot or power off?", powerDialog);
+    QPushButton *rebootButton = new QPushButton("Reboot", powerDialog);
+    QPushButton *powerOffButton = new QPushButton("Power Off", powerDialog);
+
+    connect(rebootButton, &QPushButton::clicked, [=]() {
+        qApp->exit(1);
+    });
+
+    connect(powerOffButton, &QPushButton::clicked, [=]() {
+        qApp->exit(0);
+    });
+
+    dialogLayout->addWidget(infoLabel);
+    dialogLayout->addWidget(rebootButton);
+    dialogLayout->addWidget(powerOffButton);
+
+    powerDialog->exec();
+
+    overlay->deleteLater();
+    powerDialog->deleteLater();
 }
