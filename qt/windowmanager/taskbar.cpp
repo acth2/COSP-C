006003c9584751ddc29669337fb1157f1ceaa8a0
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-04 13:36:16 +0200

    Update taskbar.cpp

--- qt/windowmanager/taskbar.cpp
+++ qt/windowmanager/taskbar.cpp
@@ -1,13 +1,15 @@
 #include "taskbar.h"
+#include <QPropertyAnimation>
 #include <QApplication>
 #include <QScreen>
+#include <QVBoxLayout>
 
 TaskBar::TaskBar(QWidget *parent) : QWidget(parent) {
     setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
     setStyleSheet("background-color: #333333;");
 
     startButton = new QPushButton(this);
-    startButton->setIcon(QIcon("/usr/cydra/cydra.png")); 
+    startButton->setIcon(QIcon("/usr/cydra/cydra.png"));
     startButton->setIconSize(QSize(32, 32));
     startButton->setStyleSheet("border: none;");
 
@@ -15,6 +17,13 @@ TaskBar::TaskBar(QWidget *parent) : QWidget(parent) {
     layout->addWidget(startButton, 0, Qt::AlignLeft | Qt::AlignBottom);
     layout->setContentsMargins(5, 5, 5, 5);
     setLayout(layout);
+
+    popup = new QLabel(this);
+    popup->setGeometry(0, -200, 200, 200);
+    popup->setStyleSheet("background-color: white; border: 1px solid gray;");
+    popup->hide();
+
+    connect(startButton, &QPushButton::clicked, this, &TaskBar::showPopup);
 
     adjustSizeToScreen();
 }
@@ -31,4 +40,14 @@ void TaskBar::adjustSizeToScreen() {
         setFixedSize(screenGeometry.width(), 40);
         move(0, screenGeometry.height() - height());
     }
+}
+
+void TaskBar::showPopup() {
+    popup->show();
+    
+    QPropertyAnimation *animation = new QPropertyAnimation(popup, "geometry");
+    animation->setDuration(500);
+    animation->setStartValue(QRect(popup->x(), popup->y(), popup->width(), popup->height()));
+    animation->setEndValue(QRect(popup->x(), 0, popup->width(), popup->height()));
+    animation->start(QAbstractAnimation::DeleteWhenStopped);
 }
