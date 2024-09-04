cb757534fd3697fc6cdce882ca93c1e0047fa633
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-04 15:44:28 +0200

    Update taskbar.h

--- qt/windowmanager/taskbar.h
+++ qt/windowmanager/taskbar.h
@@ -4,31 +4,36 @@
 #include <QWidget>
 #include <QPushButton>
 #include <QLabel>
+#include <QHBoxLayout>
 #include <QResizeEvent>
 #include <QMouseEvent>
 #include <QEvent>
+
+class WindowManager;
 
 class TaskBar : public QWidget {
     Q_OBJECT
 
 public:
-    explicit TaskBar(QWidget *parent = nullptr);
+    explicit TaskBar(WindowManager *windowManager, QWidget *parent = nullptr);
 
 protected:
     void resizeEvent(QResizeEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
     bool eventFilter(QObject *watched, QEvent *event) override;
+
 private:
     QLabel *popup;
     QPushButton *startButton;
-    void adjustSizeToScreen();
-    void hidePopup();
+    WindowManager *windowManager;
     bool popupVisible = false;
+    void adjustSizeToScreen();
     void showPopup();
-    void closePopup();
+    void hidePopup();
+    QLabel *popup;
+    QPushButton *startButton;
     void installEventFilter();
     bool isPopupVisible = false;
 };
 
 #endif // TASKBAR_H
-
