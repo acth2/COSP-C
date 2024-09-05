bf3419c39801f9a7668fce0e60af2e3d09844c66
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-05 20:45:34 +0200

    Update TerminalWindow.cpp

--- qt/windowmanager/TerminalWindow.cpp
+++ qt/windowmanager/TerminalWindow.cpp
@@ -2,55 +2,58 @@
 #include <QApplication>
 #include <QScreen>
 #include <QHBoxLayout>
+#include <QVBoxLayout>
 #include <QKeyEvent>
 #include <QLabel>
+#include <QPushButton>
+#include <QProcess>
 #include <QMenuBar>
 
-TerminalWindow::TerminalWindow(QWidget *parent) 
+TerminalWindow::TerminalWindow(QWidget *parent)
     : QMainWindow(parent), terminalProcess(new QProcess(this)), isFullScreenMode(false) {
 
     setupUI();
 
-    terminalProcess->start("xterm", QStringList() << "-into" << QString::number(centralWidget->winId()));
+    terminalProcess->start("xterm", QStringList() << "-into" << QString::number(terminalWidget->winId()));
 }
 
 void TerminalWindow::setupUI() {
     centralWidget = new QWidget(this);
-    layout = new QVBoxLayout(centralWidget);
-    layout->setContentsMargins(0, 0, 0, 0);
-    centralWidget->setLayout(layout);
+    QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
+    mainLayout->setContentsMargins(0, 0, 0, 0);
+
+    QWidget *topBar = new QWidget(this);
+    QHBoxLayout *topBarLayout = new QHBoxLayout(topBar);
+    topBarLayout->setContentsMargins(5, 5, 5, 5);
+
+    QPushButton *closeButton = new QPushButton("X", this);
+    QPushButton *fullscreenButton = new QPushButton("[ ]", this);
+
+    topBarLayout->addWidget(fullscreenButton);
+    topBarLayout->addStretch();
+    topBarLayout->addWidget(closeButton);
+
+    connect(closeButton, &QPushButton::clicked, this, &TerminalWindow::close);
+    connect(fullscreenButton, &QPushButton::clicked, this, &TerminalWindow::toggleFullScreen);
+
+    mainLayout->addWidget(topBar);
+
+    terminalWidget = new QWidget(this);
+    terminalWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    mainLayout->addWidget(terminalWidget);
 
     setCentralWidget(centralWidget);
-
-    setupCustomTitleBar();
-    
-    resize(500, 500);
-    centerWindow();
 }
 
-void TerminalWindow::setupCustomTitleBar() {
-    customTitleBar = new QWidget(this);
-    customTitleBar->setStyleSheet("background-color: #2c3e50; padding: 5px;");
-
-    QHBoxLayout *titleBarLayout = new QHBoxLayout(customTitleBar);
-    titleBarLayout->setContentsMargins(5, 0, 5, 0);
-
-    fullscreenButton = new QPushButton("⛶", this);
-    fullscreenButton->setFixedSize(30, 30);
-    connect(fullscreenButton, &QPushButton::clicked, this, &TerminalWindow::toggleFullScreen);
-    titleBarLayout->addWidget(fullscreenButton);
-
-    QLabel *titleLabel = new QLabel("Terminal", this);
-    titleLabel->setStyleSheet("color: white; font-size: 14px;");
-    titleLabel->setAlignment(Qt::AlignCenter);
-    titleBarLayout->addWidget(titleLabel);
-
-    closeButton = new QPushButton("✕", this);
-    closeButton->setFixedSize(30, 30);
-    connect(closeButton, &QPushButton::clicked, this, &TerminalWindow::close);
-    titleBarLayout->addWidget(closeButton);
-
-    layout->addWidget(customTitleBar);
+void TerminalWindow::toggleFullScreen() {
+    if (isFullScreenMode) {
+        showNormal();
+        isFullScreenMode = false;
+    } else {
+        showFullScreen();
+        isFullScreenMode = true;
+    }
 }
 
 void TerminalWindow::keyPressEvent(QKeyEvent *event) {
@@ -58,31 +61,6 @@ void TerminalWindow::keyPressEvent(QKeyEvent *event) {
         toggleFullScreen();
     } else if (event->key() == Qt::Key_Escape && isFullScreenMode) {
         toggleFullScreen();
-    } else {
-        QMainWindow::keyPressEvent(event);
     }
-}
-
-void TerminalWindow::toggleFullScreen() {
-    if (isFullScreenMode) {
-        setGeometry(normalGeometry);
-        isFullScreenMode = false;
-        menuBar()->show();
-        customTitleBar->show();
-    } else {
-        normalGeometry = geometry();
-        showFullScreen();
-        isFullScreenMode = true;
-        customTitleBar->hide();
-    }
-}
-
-void TerminalWindow::centerWindow() {
-    QScreen *screen = QApplication::primaryScreen();
-    if (screen) {
-        QRect screenGeometry = screen->geometry();
-        int x = (screenGeometry.width() - width()) / 2;
-        int y = (screenGeometry.height() - height()) / 2;
-        move(x, y);
-    }
+    QMainWindow::keyPressEvent(event);
 }
