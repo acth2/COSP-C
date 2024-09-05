b57858bbf5627bd44fa27e773c3e3dbdcb10eadd
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-05 21:06:02 +0200

    Update TerminalWindow.cpp

--- qt/windowmanager/TerminalWindow.cpp
+++ qt/windowmanager/TerminalWindow.cpp
@@ -1,68 +1,55 @@
-#include <QApplication>
-#include <QMainWindow>
-#include <QVBoxLayout>
-#include <QWidget>
+#include "TerminalWindow.h"
 #include <QPushButton>
-#include <QKeyEvent>
+#include <QVBoxLayout>
 #include <QLabel>
 
-class TerminalWindow : public QMainWindow {
-    Q_OBJECT
+TerminalWindow::TerminalWindow(QWidget *parent) 
+    : QMainWindow(parent), isFullScreenMode(false) {
+    setupUI();
+}
 
-public:
-    TerminalWindow(QWidget *parent = nullptr) : QMainWindow(parent), isFullScreenMode(false) {
-        setupUI();
+void TerminalWindow::keyPressEvent(QKeyEvent *event) {
+    if (event->key() == Qt::Key_F11) {
+        toggleFullScreen();
+    } else if (event->key() == Qt::Key_Escape && isFullScreenMode) {
+        toggleFullScreen();
     }
+    QMainWindow::keyPressEvent(event);
+}
 
-protected:
-    void keyPressEvent(QKeyEvent *event) override {
-        if (event->key() == Qt::Key_F11) {
-            toggleFullScreen();
-        } else if (event->key() == Qt::Key_Escape && isFullScreenMode) {
-            toggleFullScreen();
-        }
-        QMainWindow::keyPressEvent(event);
+void TerminalWindow::toggleFullScreen() {
+    if (isFullScreenMode) {
+        showNormal();
+        isFullScreenMode = false;
+    } else {
+        showFullScreen();
+        isFullScreenMode = true;
     }
+}
 
-private slots:
-    void toggleFullScreen() {
-        if (isFullScreenMode) {
-            showNormal();
-            isFullScreenMode = false;
-        } else {
-            showFullScreen();
-            isFullScreenMode = true;
-        }
-    }
+void TerminalWindow::setupUI() {
+    centralWidget = new QWidget(this);
+    QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
+    mainLayout->setContentsMargins(0, 0, 0, 0);
 
-private:
-    void setupUI() {
-        centralWidget = new QWidget(this);
-        QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
-        mainLayout->setContentsMargins(0, 0, 0, 0);
+    QWidget *topBar = new QWidget(this);
+    QHBoxLayout *topBarLayout = new QHBoxLayout(topBar);
+    topBarLayout->setContentsMargins(5, 5, 5, 5);
 
-        QWidget *topBar = new QWidget(this);
-        QHBoxLayout *topBarLayout = new QHBoxLayout(topBar);
-        topBarLayout->setContentsMargins(5, 5, 5, 5);
+    QPushButton *closeButton = new QPushButton("X", this);
+    QPushButton *fullscreenButton = new QPushButton("[ ]", this);
 
-        QPushButton *closeButton = new QPushButton("X", this);
-        QPushButton *fullscreenButton = new QPushButton("[ ]", this);
+    topBarLayout->addWidget(fullscreenButton);
+    topBarLayout->addStretch();
+    topBarLayout->addWidget(closeButton);
 
-        topBarLayout->addWidget(fullscreenButton);
-        topBarLayout->addStretch();
-        topBarLayout->addWidget(closeButton);
+    connect(closeButton, &QPushButton::clicked, this, &TerminalWindow::close);
+    connect(fullscreenButton, &QPushButton::clicked, this, &TerminalWindow::toggleFullScreen);
 
-        connect(closeButton, &QPushButton::clicked, this, &TerminalWindow::close);
-        connect(fullscreenButton, &QPushButton::clicked, this, &TerminalWindow::toggleFullScreen);
+    mainLayout->addWidget(topBar);
 
-        mainLayout->addWidget(topBar);
+    QLabel *label = new QLabel("Regular window", this);
+    mainLayout->addWidget(label);
 
-        QLabel *label = new QLabel("Ceci est une fenêtre test sans xterm", this);
-        mainLayout->addWidget(label);
-
-        setCentralWidget(centralWidget);
-    }
-
-    QWidget *centralWidget;
-    bool isFullScreenMode;
-};
+    setCentralWidget(centralWidget);
+}
