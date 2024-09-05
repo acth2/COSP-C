9eda781dee2ebe6c9834b71e2335b4551cc707c4
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-05 21:08:20 +0200

    Update TerminalWindow.h

--- qt/windowmanager/TerminalWindow.h
+++ qt/windowmanager/TerminalWindow.h
@@ -2,34 +2,35 @@
 #define TERMINALWINDOW_H
 
 #include <QMainWindow>
-#include <QProcess>
-#include <QVBoxLayout>
+#include <QKeyEvent>
 #include <QWidget>
 #include <QPushButton>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QProcess>
+#include <QTextEdit>
 
 class TerminalWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     explicit TerminalWindow(QWidget *parent = nullptr);
-    void toggleFullScreen();
 
 protected:
     void keyPressEvent(QKeyEvent *event) override;
 
+private slots:
+    void toggleFullScreen();
+
 private:
+    void setupUI();
+    
     QWidget *centralWidget;
-    QProcess *terminalProcess;
-    QVBoxLayout *layout;
-    QWidget *customTitleBar;
+    QWidget *topBar;
     QPushButton *closeButton;
     QPushButton *fullscreenButton;
+    QTextEdit *terminalWidget;
     bool isFullScreenMode;
-    QRect normalGeometry;
-
-    void setupUI();
-    void centerWindow();
-    void setupCustomTitleBar();
 };
 
 #endif // TERMINALWINDOW_H
