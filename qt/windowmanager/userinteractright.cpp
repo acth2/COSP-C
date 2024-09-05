d7d9e8ee6e61ee98839a7ff64a9f8d23e6d77124
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-05 12:52:52 +0200

    Update userinteractright.cpp

--- qt/windowmanager/userinteractright.cpp
+++ qt/windowmanager/userinteractright.cpp
@@ -143,6 +143,7 @@ void UserInteractRight::button1Clicked() {
     TerminalWindow *window = new TerminalWindow();
     window->show();
     window->raise();
+    window->setFixedSize(500, 500);
     window->activateWindow();
 }
 
