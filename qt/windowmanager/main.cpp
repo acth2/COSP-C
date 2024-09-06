bf1cad18ee8fda7aa35440c39d38689b06fa418c
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-06 18:13:22 +0200

    Update main.cpp

--- qt/windowmanager/main.cpp
+++ qt/windowmanager/main.cpp
@@ -5,10 +5,10 @@
 #include <QFile>
 #include <QTimer>
 
-void checkForSignalFile(Taskbar &taskBar) {
+void checkForSignalFile(TaskBar &taskBar) {
     QFile file("/usr/cydra/temp/centralSign");
     if (file.exists()) {
-        if(taskBar.isPopupVisible) {
+        if (taskBar.isPopupVisible()) {
             taskBar.closePopup();
         } else {
             taskBar.showPopup();
@@ -28,15 +28,15 @@ int main(int argc, char *argv[]) {
         QRect screenGeometry = screen->geometry();
         manager.setGeometry(screenGeometry);
     }
-    
 
     manager.setWindowTitle("CWM");
     manager.showFullScreen();
 
     QTimer timer;
-    QObject::connect(&timer, &QTimer::timeout, checkForSignalFile);
-    timer.start(500); 
-    
+
+    QObject::connect(&timer, &QTimer::timeout, [&taskBar]() { checkForSignalFile(taskBar); });
+    timer.start(500);
+
     taskBar.show();
     return app.exec();
 }
