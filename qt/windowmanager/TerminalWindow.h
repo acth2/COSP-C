2761557ad9f868d544a78198b14f3db6fe0ea05e
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-08 19:30:27 +0200

    Update TerminalWindow.h

--- qt/windowmanager/TerminalWindow.h
+++ qt/windowmanager/TerminalWindow.h
@@ -2,16 +2,8 @@
 #define TERMINALWINDOW_H
 
 #include <QMainWindow>
-#include <QPushButton>
+#include <QProcess>
 #include <QWidget>
-#include <QPlainTextEdit>
-#include <QMouseEvent>
-#include <QKeyEvent>
-#include <QProcess>
-#include <QSocketNotifier>
-#include <unistd.h>
-#include <utmp.h>
-#include <QProcess>
 
 class TerminalWindow : public QMainWindow {
     Q_OBJECT
@@ -20,40 +12,18 @@ public:
     explicit TerminalWindow(QWidget *parent = nullptr);
 
 protected:
-    void keyPressEvent(QKeyEvent *event) override;
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mouseReleaseEvent(QMouseEvent *event) override;
-
-private slots:
-    void toggleFullScreen();
-    void windowedFullScreen();
-    void executeCommand(const QString &command);
+    void resizeEvent(QResizeEvent *event) override;
+    void closeEvent(QCloseEvent *event) override;
 
 private:
-    QWidget *centralWidget;
-    QWidget *topBar;
-    QPushButton *closeButton;
-    QPushButton *fullscreenButton;
-    QPlainTextEdit *terminalWidget;
-    bool isFullScreenMode;
-    bool dragging;
-    bool isFullMode;
-    bool windowedFull;
-    bool resizing;
-    QPoint dragStartPosition;
-    QSize resizeStartSize;
-    QPoint resizeStartPosition;
-    QString currentText;
-    QString currentCommand;
-
-    int master_fd;
-    QSocketNotifier *ptyNotifier;
-
     void setupUI();
+    void startXterm();
+
+    QWidget *centralWidget;
+    QWidget *xtermWidget;
+    QProcess *xtermProcess;
+
     void updateTopBarVisibility();
-    void setupPTY();
-    void handlePTYInput();
 };
 
 #endif // TERMINALWINDOW_H
