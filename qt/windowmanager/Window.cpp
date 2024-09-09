a2dc7e4eb0152d98e10ac8e075d5eb5b81dd3225
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-09 18:11:02 +0200

    Update Window.cpp

--- qt/windowmanager/Window.cpp
+++ qt/windowmanager/Window.cpp
@@ -1,45 +1,60 @@
-#include "Window.h"
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QApplication>
-#include <QCursor>
+#include "window.h"
 #include <QMouseEvent>
-#include <QX11Info>
-#include <X11/Xlib.h>
-#include <QDebug>
+#include <QApplication>
+#include <QScreen>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QTimer>
 
-Window::Window(QWidget *parent)
-    : QMainWindow(parent), isFullScreenMode(false), dragging(false), resizing(false) {
-    monitorWindows();
+Window::Window(Display *disp, Window root, QWidget *parent)
+    : QWidget(parent), display(disp), rootWindow(root), dragging(false), isFullScreenMode(false) {
+    layout = new QVBoxLayout(this);
+    setLayout(layout);
+    addTopBar();
+
+    detectWindows();
 }
 
-void Window::monitorWindows() {
-    Display *display = QX11Info::display();
-    Window root = DefaultRootWindow(display);
+Window::~Window() {
+    XCloseDisplay(display);
+}
 
-    Window rootReturn, parentReturn;
+void Window::detectWindows() {
+    Window root_return, parent_return;
     Window *children;
-    unsigned int numChildren;
+    unsigned int nchildren;
 
-    if (XQueryTree(display, root, &rootReturn, &parentReturn, &children, &numChildren)) {
-        for (unsigned int i = 0; i < numChildren; ++i) {
-            WId windowId = children[i];
-            XWindowAttributes attributes;
-            if (XGetWindowAttributes(display, windowId, &attributes) && attributes.map_state == IsViewable) {
-                setupUI(windowId);
-            }
-        }
-        XFree(children);
+    if (XQueryTree(display, rootWindow, &root_return, &parent_return, &children, &nchildren) == 0) {
+        return;
     }
+
+    for (unsigned int i = 0; i < nchildren; i++) {
+        attachToWindow(children[i]);
+    }
+
+    XFree(children);
 }
 
-void Window::setupUI(WId targetWinId) {
-    currentWinId = targetWinId;
+void Window::attachToWindow(Window target) {
+    targetWindow = target;
 
-    // Setup custom top bar and layout
+    XWindowAttributes attributes;
+    XGetWindowAttributes(display, targetWindow, &attributes);
+
+    int barHeight = 30;
+    int newHeight = attributes.height - barHeight;
+
+    XMoveResizeWindow(display, targetWindow, attributes.x, attributes.y + barHeight, attributes.width, newHeight);
+
+    sendConfigureEvent(targetWindow, attributes.x, attributes.y + barHeight, attributes.width, newHeight);
+}
+
+void Window::addTopBar() {
     topBar = new QWidget(this);
     topBar->setFixedHeight(30);
     topBar->setStyleSheet("background-color: #333; color: white;");
+
     QHBoxLayout *topBarLayout = new QHBoxLayout(topBar);
     topBarLayout->setContentsMargins(0, 0, 0, 0);
 
@@ -50,48 +65,65 @@ void Window::setupUI(WId targetWinId) {
     topBarLayout->addStretch();
     topBarLayout->addWidget(closeButton);
 
-    connect(closeButton, &QPushButton::clicked, [this]() { close(); });
-    connect(fullscreenButton, &QPushButton::clicked, [this]() { toggleFullScreen(); });
+    layout->addWidget(topBar);
 
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    mainLayout->addWidget(topBar);
-    setLayout(mainLayout);
-
-    moveWindow(targetWinId);
-    resizeWindow(targetWinId);
+    connect(closeButton, &QPushButton::clicked, this, &Window::closeWindow);
+    connect(fullscreenButton, &QPushButton::clicked, this, &Window::toggleFullScreen);
 }
 
-void Window::moveWindow(WId targetWinId) {
-    connect(topBar, &QWidget::mousePressEvent, this, [this, targetWinId](QMouseEvent *event) {
-        if (event->button() == Qt::LeftButton) {
-            dragging = true;
-            dragStartPosition = event->globalPos();
-        }
-    });
-
-    connect(topBar, &QWidget::mouseMoveEvent, this, [this, targetWinId](QMouseEvent *event) {
-        if (dragging) {
-            QPoint delta = event->globalPos() - dragStartPosition;
-            dragStartPosition = event->globalPos();
-
-            XMoveWindow(QX11Info::display(), targetWinId, delta.x(), delta.y());
-        }
-    });
-
-    connect(topBar, &QWidget::mouseReleaseEvent, this, [this]() {
-        dragging = false;
-    });
-}
-
-void Window::resizeWindow(WId targetWinId) {
-    // Not implemented for now
-    // Maybe soon maybe maybe...
+void Window::closeWindow() {
+    if (targetWindow) {
+        XDestroyWindow(display, targetWindow);
+    }
+    close();
 }
 
 void Window::toggleFullScreen() {
     if (isFullScreenMode) {
+        showNormal();
         isFullScreenMode = false;
     } else {
+        QScreen *screen = QApplication::primaryScreen();
+        QRect screenGeometry = screen->geometry();
+        setGeometry(screenGeometry);
+        showFullScreen();
         isFullScreenMode = true;
     }
+}
+
+void Window::mousePressEvent(QMouseEvent *event) {
+    dragging = true;
+    dragStartPosition = event->globalPos() - frameGeometry().topLeft();
+}
+
+void Window::mouseMoveEvent(QMouseEvent *event) {
+    if (dragging) {
+        move(event->globalPos() - dragStartPosition);
+    }
+}
+
+void Window::mouseReleaseEvent(QMouseEvent *event) {
+    dragging = false;
+}
+
+void Window::resizeEvent(QResizeEvent *event) {
+    // Handle resizing logic
+}
+
+void Window::sendConfigureEvent(Window win, int x, int y, int width, int height) {
+    XConfigureEvent configureEvent;
+    configureEvent.type = ConfigureNotify;
+    configureEvent.display = display;
+    configureEvent.event = win;
+    configureEvent.window = win;
+    configureEvent.x = x;
+    configureEvent.y = y;
+    configureEvent.width = width;
+    configureEvent.height = height;
+    configureEvent.border_width = 0;
+    configureEvent.above = None;
+    configureEvent.override_redirect = False;
+
+    XSendEvent(display, win, False, StructureNotifyMask, (XEvent*)&configureEvent);
+    XFlush(display);
 }
