bd6c72b23f62bf7c4b1810d2d1b8488aff31402e
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-09 13:59:13 +0200

    Update TerminalWindow.cpp

--- qt/windowmanager/TerminalWindow.cpp
+++ qt/windowmanager/TerminalWindow.cpp
@@ -1,64 +1,41 @@
-#include <QApplication>
-#include <QMainWindow>
-#include <QWidget>
+#include "TerminalWindow.h"
 #include <QVBoxLayout>
-#include <QPushButton>
+#include <QHBoxLayout>
+#include <QApplication>
 #include <QProcess>
+#include <QScreen>
+#include <QCursor>
+#include <QMouseEvent>
+#include <QKeyEvent>
+#include <QFocusEvent>
 #include <QTimer>
 #include <QResizeEvent>
-#include <QMouseEvent>
-#include <QScreen>
-
-class TerminalWindow : public QMainWindow {
-    Q_OBJECT
-
-public:
-    TerminalWindow(QWidget *parent = nullptr);
-
-private slots:
-    void launchXTerm();
-    void resizeXTerm();
-    void toggleFullScreen();
-
-private:
-    void setupUI();
-    void resizeEvent(QResizeEvent *event) override;
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mouseReleaseEvent(QMouseEvent *event) override;
-
-    QWidget *centralWidget;
-    QWidget *xtermWidget;
-    QProcess *xtermProcess;
-    QPushButton *closeButton;
-    QPushButton *fullscreenButton;
-    bool isFullScreenMode;
-    QPoint dragStartPosition;
-    bool dragging;
-};
+#include <QDebug>  // Added for debugging
 
 TerminalWindow::TerminalWindow(QWidget *parent)
-    : QMainWindow(parent), isFullScreenMode(false), dragging(false), xtermProcess(new QProcess(this)) {
+    : QMainWindow(parent), 
+      isFullScreenMode(false), 
+      dragging(false), 
+      resizing(false),
+      xtermProcess(new QProcess(this)) {
     setupUI();
-    QTimer::singleShot(100, this, &TerminalWindow::launchXTerm);
+    setCursor(Qt::ArrowCursor);
 
-    // Set up a timer to resize xterm
-    QTimer *resizeTimer = new QTimer(this);
-    connect(resizeTimer, &QTimer::timeout, this, &TerminalWindow::resizeXTerm);
-    resizeTimer->start(500); // Resize every 500ms
+    qDebug() << "TerminalWindow initialized";
+    QTimer::singleShot(100, this, &TerminalWindow::launchXTerm);
 }
 
 void TerminalWindow::setupUI() {
     centralWidget = new QWidget(this);
-    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
-    layout->setContentsMargins(0, 0, 0, 0);
+    QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
+    mainLayout->setContentsMargins(0, 0, 0, 0);
 
-    // Top bar for buttons
-    QWidget *topBar = new QWidget(this);
+    topBar = new QWidget(this);
     topBar->setFixedHeight(30);
     topBar->setStyleSheet("background-color: #333; color: white;");
-
     QHBoxLayout *topBarLayout = new QHBoxLayout(topBar);
+    topBarLayout->setContentsMargins(0, 0, 0, 0);
+
     closeButton = new QPushButton("✕", topBar);
     fullscreenButton = new QPushButton("❐", topBar);
 
@@ -69,40 +46,103 @@ void TerminalWindow::setupUI() {
     connect(closeButton, &QPushButton::clicked, this, &TerminalWindow::close);
     connect(fullscreenButton, &QPushButton::clicked, this, &TerminalWindow::toggleFullScreen);
 
-    layout->addWidget(topBar);
-
-    // xterm widget (this is where xterm will be embedded)
     xtermWidget = new QWidget(this);
-    layout->addWidget(xtermWidget);
+    xtermWidget->setStyleSheet("background-color: black;");
+
+    mainLayout->addWidget(topBar);
+    mainLayout->addWidget(xtermWidget);
 
     setCentralWidget(centralWidget);
     setWindowTitle("Terminal Window");
+
+    updateTopBarVisibility();
 }
 
 void TerminalWindow::launchXTerm() {
-    WId winId = xtermWidget->winId();
+    if (!xtermWidget) {
+        qDebug() << "xtermWidget is null!";
+        return;
+    }
 
+    WId winId = xtermWidget->winId();
     QString program = "xterm";
     QStringList arguments;
     arguments << "-into" << QString::number(winId) << "-geometry" << "80x24";
 
+    qDebug() << "Launching xterm with arguments:" << arguments;
+
     xtermProcess->start(program, arguments);
+    if (!xtermProcess->waitForStarted()) {
+        qDebug() << "Failed to start xterm!";
+    }
 }
 
 void TerminalWindow::resizeEvent(QResizeEvent *event) {
     QMainWindow::resizeEvent(event);
-    resizeXTerm();
+
+    if (xtermProcess->state() == QProcess::Running) {
+        resizeXTerm();
+    }
 }
 
 void TerminalWindow::resizeXTerm() {
     if (xtermProcess->state() == QProcess::Running) {
-        int columns = this->width() / 9;
-        int rows = this->height() / 18;
+        int newColumns = xtermWidget->width() / 9;
+        int newRows = xtermWidget->height() / 18;
 
-        QString resizeCommand = QString("printf '\\e[8;%1;%2t'").arg(rows).arg(columns);
+        QString resizeCommand = QString("printf '\\e[8;%1;%2t'").arg(newRows).arg(newColumns);
         xtermProcess->write(resizeCommand.toUtf8());
         xtermProcess->waitForBytesWritten();
     }
+}
+
+void TerminalWindow::mousePressEvent(QMouseEvent *event) {
+    int margin = 10;
+    bool onRightEdge = event->x() > (width() - margin);
+    bool onBottomEdge = event->y() > (height() - margin);
+
+    if (onRightEdge || onBottomEdge) {
+        resizing = true;
+        resizeStartPosition = event->globalPos();
+        resizeStartSize = size();
+    } else {
+        dragging = true;
+        dragStartPosition = event->globalPos() - frameGeometry().topLeft();
+    }
+    QMainWindow::mousePressEvent(event);
+}
+
+void TerminalWindow::mouseMoveEvent(QMouseEvent *event) {
+    int margin = 35;
+    int marginIconing = 20;
+
+    bool onRightEdge = event->x() > (width() - marginIconing);
+    bool onBottomEdge = event->y() > (height() - marginIconing);
+
+    if (onRightEdge && onBottomEdge) {
+        setCursor(Qt::SizeFDiagCursor);
+    } else if (onRightEdge) {
+        setCursor(Qt::SizeHorCursor);
+    } else if (onBottomEdge) {
+        setCursor(Qt::SizeVerCursor);
+    } else {
+        setCursor(Qt::ArrowCursor);
+    }
+
+    if (resizing) {
+        QSize newSize = resizeStartSize + QSize(event->globalPos().x() - resizeStartPosition.x(),
+                                                event->globalPos().y() - resizeStartPosition.y());
+        resize(newSize);
+    } else if (dragging) {
+        move(event->globalPos() - dragStartPosition);
+    }
+    QMainWindow::mouseMoveEvent(event);
+}
+
+void TerminalWindow::mouseReleaseEvent(QMouseEvent *event) {
+    resizing = false;
+    dragging = false;
+    QMainWindow::mouseReleaseEvent(event);
 }
 
 void TerminalWindow::toggleFullScreen() {
@@ -116,19 +156,24 @@ void TerminalWindow::toggleFullScreen() {
         showFullScreen();
         isFullScreenMode = true;
     }
+    updateTopBarVisibility();
 }
 
-void TerminalWindow::mousePressEvent(QMouseEvent *event) {
-    dragging = true;
-    dragStartPosition = event->globalPos() - frameGeometry().topLeft();
+void TerminalWindow::updateTopBarVisibility() {
+    topBar->setVisible(!isFullScreenMode);
 }
 
-void TerminalWindow::mouseMoveEvent(QMouseEvent *event) {
-    if (dragging) {
-        move(event->globalPos() - dragStartPosition);
+void TerminalWindow::closeEvent(QCloseEvent *event) {
+    if (xtermProcess->state() == QProcess::Running) {
+        xtermProcess->terminate();
+        xtermProcess->waitForFinished();
     }
+    QMainWindow::closeEvent(event);
 }
 
-void TerminalWindow::mouseReleaseEvent(QMouseEvent *event) {
-    dragging = false;
+void TerminalWindow::focusInEvent(QFocusEvent *event) {
+    if (xtermProcess && xtermProcess->state() == QProcess::Running) {
+        xtermWidget->setFocus(); 
+    }
+    QMainWindow::focusInEvent(event);
 }
