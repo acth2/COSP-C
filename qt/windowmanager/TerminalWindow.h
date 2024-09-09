2e40f9a7dab2ed951519a60c6c97a9e3f25f8d62
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-09 13:55:40 +0200

    Update TerminalWindow.h

--- qt/windowmanager/TerminalWindow.h
+++ qt/windowmanager/TerminalWindow.h
@@ -2,11 +2,12 @@
 #define TERMINALWINDOW_H
 
 #include <QMainWindow>
-#include <QPushButton>
 #include <QWidget>
+#include <QPushButton>
 #include <QProcess>
 #include <QResizeEvent>
-#include <QFocusEvent>
+#include <QMouseEvent>
+#include <QTimer>
 
 class TerminalWindow : public QMainWindow {
     Q_OBJECT
@@ -14,42 +15,26 @@ class TerminalWindow : public QMainWindow {
 public:
     explicit TerminalWindow(QWidget *parent = nullptr);
 
-protected:
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mouseReleaseEvent(QMouseEvent *event) override;
-    void resizeEvent(QResizeEvent *event) override;
-    void closeEvent(QCloseEvent *event) override;
-    void focusInEvent(QFocusEvent *event) override;
-
 private slots:
+    void launchXTerm();
+    void resizeXTerm();
     void toggleFullScreen();
-    void windowedFullScreen();
-    void executeCommand(const QString &command);
 
 private:
     void setupUI();
-    void launchXTerm();
-    void updateTopBarVisibility();
-    void resizeXTerm();
+    void resizeEvent(QResizeEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
 
-    QWidget *topBar;
-    QPushButton *closeButton;
-    QPushButton *fullscreenButton;
-    QProcess *terminalProcess;
     QWidget *centralWidget;
     QWidget *xtermWidget;
     QProcess *xtermProcess;
+    QPushButton *closeButton;
+    QPushButton *fullscreenButton;
     bool isFullScreenMode;
-    bool dragging;
-    bool resizing;
     QPoint dragStartPosition;
-    QSize resizeStartSize;
-    QPoint resizeStartPosition;
-    bool isFullMode;
-    bool windowedFull;
-    QString currentText;
-    QString currentCommand;
+    bool dragging;
 };
 
 #endif // TERMINALWINDOW_H
