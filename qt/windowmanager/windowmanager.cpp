586b0d2a5f261ce15080c80fae896a4c9e581cdd
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-11 20:46:10 +0200

    Update windowmanager.cpp

--- qt/windowmanager/windowmanager.cpp
+++ qt/windowmanager/windowmanager.cpp
@@ -99,6 +99,73 @@ void WindowManager::listExistingWindows() {
     }
 }
 
+void WindowManager::checkForNewWindows() {
+    xDisplay = XOpenDisplay(nullptr);
+    listExistingWindows();
+    processX11Events(); 
+}
+
+void WindowManager::listExistingWindows() {
+    Atom netWmWindowType = XInternAtom(xDisplay, "_NET_WM_WINDOW_TYPE", False);
+    Atom netWmWindowTypeDesktop = XInternAtom(xDisplay, "_NET_WM_WINDOW_TYPE_DESKTOP", False);
+
+    Window windowRoot = DefaultRootWindow(xDisplay);
+    Window parent, *children;
+    unsigned int nChildren;
+
+    if (XQueryTree(xDisplay, windowRoot, &windowRoot, &parent, &children, &nChildren)) {
+        for (unsigned int i = 0; i < nChildren; i++) {
+            Window child = children[i];
+
+            Atom type;
+            int format;
+            unsigned long nItems, bytesAfter;
+            unsigned char *data = nullptr;
+
+            if (XGetWindowProperty(xDisplay, child, netWmWindowType, 0, 1, False, XA_ATOM,
+                                   &type, &format, &nItems, &bytesAfter, &data) == Success) {
+                if (data) {
+                    Atom *atoms = (Atom *)data;
+                    if (atoms[0] == netWmWindowTypeDesktop) {
+                        XFree(data);
+                        continue;
+                    }
+                    XFree(data);
+                }
+            }
+
+            if (!trackedWindows.contains(child)) {
+                createAndTrackWindow(child);
+                trackWindowEvents(child);  
+            }
+        }
+        XFree(children);
+    }
+}
+
+void WindowManager::trackWindowEvents(Window xorgWindowId) {
+    xDisplay = XOpenDisplay(nullptr);
+    XSelectInput(xDisplay, xorgWindowId, StructureNotifyMask);
+}
+
+void WindowManager::processX11Events() {
+    XEvent event;
+    while (XPending(xDisplay)) {
+        XNextEvent(xDisplay, &event);
+        if (event.type == ConfigureNotify) {
+            XConfigureEvent xce = event.xconfigure;
+            if (trackedWindows.contains(xce.window)) {
+                QWindow *window = trackedWindows.value(xce.window);
+                appendLog(QString("Window resized/moved: (%1, %2), Size: (%3x%4)")
+                    .arg(xce.x).arg(xce.y)
+                    .arg(xce.width).arg(xce.height));
+
+                updateTaskbarPosition(window);
+            }
+        }
+    }
+}
+
 void WindowManager::toggleConsole() {
     isConsoleVisible = !isConsoleVisible;
     logLabel->setVisible(isConsoleVisible);
