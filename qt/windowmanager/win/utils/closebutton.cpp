4150515dc5973c67130edf7fa80373b7089e53bd
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-15 11:37:09 +0200

    Update closebutton.cpp

--- qt/windowmanager/win/utils/closebutton.cpp
+++ qt/windowmanager/win/utils/closebutton.cpp
@@ -5,7 +5,7 @@ CloseButton::CloseButton(QWindow *trackedWindow, QWidget *parent)
     : QPushButton(parent), trackedWindow(trackedWindow) {
 
     setText("✕");
-    setFixedSize(30, 30);
+    setFixedSize(30, 27);
 
     connect(this, &QPushButton::clicked, this, &CloseButton::handleButtonClicked);
 }
