4034f2a777bcade50a0f0f56b72c6ad8555e99ad
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-19 13:16:30 +0200

    Update topbar.cpp

--- qt/windowmanager/win/topbar.cpp
+++ qt/windowmanager/win/topbar.cpp
@@ -124,9 +124,18 @@ bool TopBar::eventFilter(QObject *obj, QEvent *event) {
 
 void TopBar::paintEvent(QPaintEvent *event) {
     Q_UNUSED(event);
-    
+
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
+
+    QRegion region(this->rect());
+    for (auto* child : this->children()) {
+        if (auto* widgetChild = qobject_cast<QWidget*>(child)) {
+            region -= widgetChild->geometry();
+        }
+    }
+
+    painter.setClipRegion(region);
 
     QRect backgroundRect = this->rect();
     painter.setBrush(QColor(0, 0, 0, 150));
@@ -136,7 +145,7 @@ void TopBar::paintEvent(QPaintEvent *event) {
     QColor glassColor(255, 255, 255, 80);
     painter.fillRect(backgroundRect, glassColor);
 
-    painter.setOpacity(1.0);
+    painter.setClipping(false);
 
     QWidget::paintEvent(event);
 }
