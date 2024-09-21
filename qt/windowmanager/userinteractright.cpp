dc60709075f41057697258718291bcca5e13a68d
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-21 14:42:57 +0200

    Update userinteractright.cpp

--- qt/windowmanager/userinteractright.cpp
+++ qt/windowmanager/userinteractright.cpp
@@ -144,7 +144,7 @@ void UserInteractRight::paintEvent(QPaintEvent *event) {
 void UserInteractRight::button1Clicked() {
     QProcess *xtermProcess = new QProcess(this);
 
-    QString program = "xterm";
+    QString program = "uxterm";
     QStringList arguments;
 
     xtermProcess->start(program, arguments);
