d533ceb204fa5be6b54775974e9baa6d11483e33
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-22 02:26:26 +0200

    Update loginwindow.cpp

--- qt/loginmanager/src/loginwindow.cpp
+++ qt/loginmanager/src/loginwindow.cpp
@@ -5,31 +5,18 @@
 #include <QTimer>
 #include <QApplication>
 #include <QDesktopWidget>
-#include <QCryptographicHash>
-#include <QPushButton>
-#include <QCursor>
-#include <QProcess>
 #include <cstdlib>
-#include <crypt.h>
-#include <shadow.h>
-#include <unistd.h>
+#include <cstdio>
+#include <memory>
+#include <array>
 
 LoginWindow::LoginWindow(QWidget *parent) : QWidget(parent) {
     QVBoxLayout *layout = new QVBoxLayout(this);
-
-    messageLabel = new QLabel();
-    messageLabel->setAlignment(Qt::AlignCenter);
-    layout->addWidget(messageLabel);
-
     QLabel *userLabel = new QLabel("Username:");
-    userLabel->setAlignment(Qt::AlignCenter);
     usernameField = new QLineEdit;
-    
     QLabel *passLabel = new QLabel("Password:");
-    passLabel->setAlignment(Qt::AlignCenter);
     passwordField = new QLineEdit;
     passwordField->setEchoMode(QLineEdit::Password);
-    
     loginButton = new QPushButton("Login");
 
     layout->addWidget(userLabel);
@@ -45,11 +32,6 @@ LoginWindow::LoginWindow(QWidget *parent) : QWidget(parent) {
     int screenHeight = QApplication::desktop()->screen()->height();
     move((screenWidth - windowSize.width()) / 2, (screenHeight - windowSize.height()) / 2);
     resize(windowSize);
-
-    QTimer::singleShot(100, this, [=]() {
-        QCursor::setPos(QCursor::pos().x() + (screenWidth - windowSize.width()) / 2 + windowSize.width() / 2,
-                         QCursor::pos().y() + (screenHeight - windowSize.height()) / 2 + windowSize.height() / 2);
-    });
 }
 
 void LoginWindow::onLoginClicked() {
@@ -59,34 +41,29 @@ void LoginWindow::onLoginClicked() {
     authenticateUser(username, password);
 }
 
-void LoginWindow::showMessage(const QString &message, bool isError) {
-    messageLabel->setText(message);
-    messageLabel->setStyleSheet(isError ? "color: red;" : "color: green;");
+QString LoginWindow::executeCommand(const QString &command) {
+    std::array<char, 128> buffer;
+    std::string result;
+
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.toStdString().c_str(), "r"), pclose);
+    if (!pipe) return "popen() failed!";
+
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return QString::fromStdString(result);
 }
 
 void LoginWindow::authenticateUser(const QString &username, const QString &password) {
-    if (username.isEmpty() || password.isEmpty()) {
-        QTimer::singleShot(3000, this, [=]() {
-            showMessage("Username and password cannot be empty.", true);
-        });
-        return;
-    }
+    QString command = "echo " + username + ":" + password + " | /usr/bin/login";
 
-    struct spwd *userInfo = getspnam(username.toStdString().c_str());
-    if (!userInfo) {
-        QTimer::singleShot(3000, this, [=]() {
-            showMessage("Invalid username or password.", true);
-        });
-        return;
-    }
+    QString result = executeCommand(command);
 
-    const char *hashedPassword = crypt(password.toStdString().c_str(), userInfo->sp_pwdp);
-
-    if (hashedPassword && QString(hashedPassword) == QString(userInfo->sp_pwdp)) {
-        showMessage("Welcome!", false);
+    if (result.contains("success")) {
+        QMessageBox::information(this, "Login Successful", "Welcome!");
     } else {
         QTimer::singleShot(3000, this, [=]() {
-            showMessage("Invalid username or password.", true);
+            QMessageBox::warning(this, "Login Failed", "Invalid username or password.");
         });
     }
 }
