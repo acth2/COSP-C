e7b41502671560a12ea59b1cbc32ec8ab9b010ce
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-24 20:20:23 +0200

    Update topbar.h

--- qt/windowmanager/win/topbar.h
+++ qt/windowmanager/win/topbar.h
@@ -55,18 +55,6 @@ private:
     bool isMaximized;
 
     bool isDarkMode;
-
-    QWidget *rightResizeHandle;
-    QWidget *leftResizeHandle;
-    QWidget *bottomResizeHandle;
-    QPoint resizeStartPos;
-    bool resizingRight = false;
-    bool resizingLeft = false;
-    bool resizingBottom = false;
-
-    void handleResizeRight(const QPoint &mousePos);
-    void handleResizeLeft(const QPoint &mousePos);
-    void handleResizeBottom(const QPoint &mousePos);
 };
 
 #endif // TOPBAR_H
