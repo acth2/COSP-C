e10b051d1252e3d38073307ca9c2b73c90daafb6
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-25 20:34:50 +0200

    Update userinteractright.h

--- qt/windowmanager/userinteractright.h
+++ qt/windowmanager/userinteractright.h
@@ -1,85 +1,37 @@
-#ifndef WINDOWMANAGER_H
-#define WINDOWMANAGER_H
+#ifndef USERINTERACTRIGHT_H
+#define USERINTERACTRIGHT_H
 
-#include <QtCore/qtextstream.h>
-#include "win/topbar.h"
 #include <QWidget>
+#include <QPushButton>
 #include <QLabel>
-#include <QSet>
-#include <QTimer>
-#include <QMap>
-#include <QResizeEvent>
-#include "taskbar.h"
-#include "konami_code_handler.h"
-#include "userinteractright.h"
-#include <X11/Xlib.h>
+#include <QMouseEvent>
+#include <QPaintEvent>
 
-class TopBar;
-
-class WindowManager : public QWidget {
+class UserInteractRight : public QWidget {
     Q_OBJECT
 
 public:
-    explicit WindowManager(QWidget *parent = nullptr);
-    void appendLog(const QString &message);
-    QMap<WId, TopBar*> windowTopBars;
-    void closeWindow(WId xorgWindowId);
+    explicit UserInteractRight(QWidget *parent = nullptr);
 
 protected:
-    bool event(QEvent *event) override;
-    void keyPressEvent(QKeyEvent *event) override;
-    void closeEvent(QCloseEvent *event) override;
-    void paintEvent(QPaintEvent *event) override;
-    void resizeEvent(QResizeEvent *event) override;
-    void updateTaskbarPosition(QWindow* window);
-    void trackWindowEvents(Window xorgWindowId);
-    void centerWindow(QWindow *window);
-    void removeCloseButton(WId windowId);
-
-    bool eventFilter(QObject *object, QEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
 
 private slots:
-    void checkForNewWindows();
-    void toggleConsole();
-    void processX11Events();
-    void cleanUpClosedWindows();
+    void button1Clicked();
 
 private:
+    QPushButton *button1;
+    QPushButton *button2;
+    QPushButton *button3;
+    QLabel *textLabel;
+    bool isMousePressed = false;
+    bool isDarkMode;
 
-    QString backgroundImagePath;
-    QLabel *logLabel;
-    QSet<QString> loggedMessages;
-    KonamiCodeHandler *konamiCodeHandler;
-    bool isConsoleVisible;
-    UserInteractRight *userInteractRightWidget;
-
-    void createAndTrackWindow(WId xorgWindowId);
-    void listExistingWindows();
-    QMap<WId, QWindow*> trackedWindows;
-    QMap<QWindow*, TaskBar*> windowTaskbars;
-    QTimer *windowCheckTimer;
-    QTimer *resizeWindowCubesTimer;
-    struct TrackingSquares {
-        QLabel *leftSquare;
-        QLabel *rightSquare;
-        QLabel *bottomSquare;
-    };
-    QMap<WId, TrackingSquares> windowSquares;
-
-    void createTrackingSquares(WId windowId);
-    void updateTrackingSquares(WId windowId);
-
-    void setupCloseButton(QWindow *window);
-    void setSupportingWMCheck();
-
-    bool resizeMode;
-    QPoint lastMousePosition;
-    WId resizingWindowId;
-
-    void onLoop();
-    Window root;
+    void setupUI();
+    void applyStyles();
+    void closeIfClickedOutside(QMouseEvent *event);
 };
 
-#endif // WINDOWMANAGER_H
+#endif // USERINTERACTRIGHT_H
