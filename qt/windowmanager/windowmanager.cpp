d515eb2af984a2006884298570eafeed79fe50da
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-27 23:12:08 +0200

    Update windowmanager.cpp

--- qt/windowmanager/windowmanager.cpp
+++ qt/windowmanager/windowmanager.cpp
@@ -282,7 +282,17 @@ void WindowManager::createAndTrackWindow(WId xorgWindowId) {
     createTrackingSquares(xorgWindowId);
 
     resizeWindowCubesTimer = new QTimer(this);
-    updateTrackingSquares(xorgWindowId);
+    connect(resizeWindowCubesTimer, &QTimer::timeout, this, [this, xorgWindowId, topBar, x11Window]() { 
+    if (x11Window) {
+        updateTrackingSquares(xorgWindowId);
+    } else {
+        return;
+    }
+        topBar->updatePosition();
+    });
+    resizeWindowCubesTimer->start(1);
+
+    topBar->updatePosition();
     topBar->updatePosition();
 }
 
