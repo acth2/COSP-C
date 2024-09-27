e4d5055936abb41773fd22f5f99d027c77667d9f
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-27 23:19:49 +0200

    Create resizecubes.h

---
+++ qt/windowmanager/win/utils/resizecubes.h
@@ -0,0 +1,27 @@
+#ifndef RESIZECUBES_H
+#define RESIZECUBES_H
+
+#include <QMap>
+#include <QLabel>
+#include <X11/Xlib.h>
+#include <QWidget>
+
+class ResizeCubes {
+public:
+    explicit ResizeCubes(QWidget *parent = nullptr);
+    void createTrackingSquares(WId windowId, const QRect &geometry);
+    void updateTrackingSquares(WId windowId, const QRect &geometry);
+    void killTrackingCubes();
+
+private:
+    struct TrackingSquares {
+        QLabel *leftSquare;
+        QLabel *rightSquare;
+        QLabel *bottomSquare;
+    };
+
+    QMap<WId, TrackingSquares> windowSquares;
+    QWidget *parentWidget;
+};
+
+#endif // RESIZECUBES_H
