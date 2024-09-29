7a73a19c6604674442402b14a66ac78b6bdf43eb
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-09-29 14:57:54 +0200

    Update windowmanager.h

--- qt/windowmanager/windowmanager.h
+++ qt/windowmanager/windowmanager.h
@@ -38,7 +38,6 @@ protected:
     void trackWindowEvents(Window xorgWindowId);
     void centerWindow(QWindow *window);
     void removeCloseButton(WId windowId);
-    void isEdgeClick(QRect &windowGeometry, QPoint &clickPosition, int edgeThreshold)
 
 private slots:
     void checkForNewWindows();
