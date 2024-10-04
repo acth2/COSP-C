9238a1574ff1cc58a10d4b3b9d4ac347e931d401
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-04 20:57:20 +0200

    Update topbar.cpp

--- qt/windowmanager/win/topbar.cpp
+++ qt/windowmanager/win/topbar.cpp
@@ -26,12 +26,8 @@ TopBar::TopBar(QWindow *parentWindow, WindowManager *manager, QWidget *parent)
     trackedWindow->installEventFilter(this);
     isMaximized = false;
 
-    QWidget *container = QWidget::createWindowContainer(trackedWindow, this);
-    container->setFocusPolicy(Qt::TabFocus);
-
     setWindowFlags(Qt::FramelessWindowHint);
     setAttribute(Qt::WA_TranslucentBackground);
-    setParent(container);
     setFocusPolicy(Qt::NoFocus);
 
     titleLabel = new QLabel(this);
