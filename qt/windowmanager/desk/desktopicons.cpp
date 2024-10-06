ae2dba8281f286676781ef928df5d5fe866a609b
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-06 10:38:36 +0200

    Update desktopicons.cpp

--- qt/windowmanager/desk/desktopicons.cpp
+++ qt/windowmanager/desk/desktopicons.cpp
@@ -5,16 +5,11 @@
 #include <QDesktopServices>
 #include <QUrl>
 #include <QIcon>
-#include <filesystem>
-#include <iostream>
-#include <QVBoxLayout>
-#include <QDir>
-#include <QDebug>
-#include <QString>
 #include <QProcessEnvironment>
-
-namespace fs = std::filesystem;
-
+#include <QDebug>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
 class DesktopIcons : public QWidget {
 public:
     DesktopIcons(QWidget *parent = nullptr) : QWidget(parent) {
@@ -25,48 +20,70 @@ public:
         QString homeDir = QProcessEnvironment::systemEnvironment().value("HOME");
         QString desktopPath = homeDir + "/A2WM/Desktop";
 
-        if (!fs::exists(desktopPath.toStdString())) {
+        DIR *dir = opendir(desktopPath.toUtf8().constData());
+        if (!dir) {
             qDebug() << "Directory not found: " << desktopPath;
             return;
         }
 
-        QGridLayout *gridLayout = new QGridLayout(this);
+        struct dirent *entry;
+        QGridLayout *gridLayout = new QGridLayout();
         int row = 0, col = 0;
         int maxCols = 5;
 
-        for (const auto &entry : fs::directory_iterator(desktopPath.toStdString())) {
-            QString filePath = QString::fromStdString(entry.path().string());
-            QString fileName = QString::fromStdString(entry.path().filename().string());
+        while ((entry = readdir(dir)) != nullptr) {
+            QString fileName = QString::fromUtf8(entry->d_name);
+            if (fileName == "." || fileName == "..")
+                continue;
+
+            QString filePath = desktopPath + "/" + fileName;
+
+            struct stat fileStat;
+            if (stat(filePath.toUtf8().constData(), &fileStat) == -1) {
+                continue;
+            }
 
             QPushButton *iconButton = new QPushButton(this);
             iconButton->setFixedSize(32, 32);
             iconButton->setIconSize(QSize(32, 32));
 
-            if (entry.is_directory()) {
+            if (S_ISDIR(fileStat.st_mode)) {
                 iconButton->setIcon(QIcon("/usr/cydra/icons/folder.png"));
-                connect(iconButton, &QPushButton::clicked, [=]() {
+                connect(iconButton, &QPushButton::clicked, [filePath]() {
                     QDesktopServices::openUrl(QUrl::fromLocalFile(filePath));
                 });
-            } else if (entry.is_regular_file()) {
+            } else if (S_ISREG(fileStat.st_mode)) {
                 iconButton->setIcon(QIcon("/usr/cydra/icons/file.png"));
-                connect(iconButton, &QPushButton::clicked, [=]() {
+                connect(iconButton, &QPushButton::clicked, [filePath]() {
                     QDesktopServices::openUrl(QUrl::fromLocalFile(filePath));
                 });
+            } else {
+                continue;
             }
 
             QLabel *nameLabel = new QLabel(fileName, this);
             nameLabel->setStyleSheet("QLabel { color : white; }");
+            nameLabel->setAlignment(Qt::AlignCenter);
 
-            gridLayout->addWidget(iconButton, row, col);
-            gridLayout->addWidget(nameLabel, row + 1, col);
+            QVBoxLayout *iconLayout = new QVBoxLayout();
+            iconLayout->addWidget(iconButton, 0, Qt::AlignCenter);
+            iconLayout->addWidget(nameLabel, 0, Qt::AlignCenter);
+
+            QWidget *iconWidget = new QWidget(this);
+            iconWidget->setLayout(iconLayout);
+
+            gridLayout->addWidget(iconWidget, row, col);
 
             col++;
             if (col >= maxCols) {
                 col = 0;
-                row += 2;
+                row++;
             }
         }
 
+        closedir(dir);
+
+        // Set the grid layout
         this->setLayout(gridLayout);
     }
 };
