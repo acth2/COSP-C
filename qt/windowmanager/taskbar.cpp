b0d9ee54f0e4f300a0a89837d6e60cd675723d6e
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-12 18:54:30 +0200

    Update taskbar.cpp

--- qt/windowmanager/taskbar.cpp
+++ qt/windowmanager/taskbar.cpp
@@ -64,33 +64,10 @@ TaskBar::TaskBar(QWidget *parent) : QWidget(parent) {
 
     connect(powerButton, &QPushButton::clicked, this, &TaskBar::showPowerMenu);
     connect(startButton, &QPushButton::clicked, this, &TaskBar::showPopup);
-    connect(startButton, &QPushButton::clicked, this, &TaskBar::toggleWindowVisibility);
 
     setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
     adjustSizeToScreen();
     installEventFilter();
-}
-
-void TaskBar::addMinimizedWindow(QWindow *window) {
-    QPushButton *windowButton = new QPushButton(this);
-
-    QIcon windowIcon = window->icon();
-    windowButton->setIcon(windowIcon);
-    windowButton->setIconSize(QSize(32, 32));
-
-    windowButton->setStyleSheet("border: none;");
-
-    minimizedLayout->addWidget(windowButton);
-
-    connect(windowButton, &QPushButton::clicked, [=]() {
-        restoreMinimizedWindow(window);
-    });
-}
-
-void TaskBar::restoreMinimizedWindow(QWindow *window) {
-    window->show();
-    window->raise();
-    window->requestActivate();
 }
 
 void TaskBar::resizeEvent(QResizeEvent *event) {
