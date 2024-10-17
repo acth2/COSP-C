2d16bd993cde99afdb50416ad2cb18051a6df465
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-17 10:26:09 +0200

    Update MinimizedPosInt.cpp

--- qt/windowmanager/win/MinimizedPosInt.cpp
+++ qt/windowmanager/win/MinimizedPosInt.cpp
@@ -1,9 +1,5 @@
 #include "MinimizedPosInt.h"
 
-MinimizedPosInt::MinimizedPosInt() {
-    sharedInt = std::make_shared<int>(75);
-}
-
 int MinimizedPosInt::getValue() {
     std::lock_guard<std::mutex> lock(mtx);
     return *sharedInt;
