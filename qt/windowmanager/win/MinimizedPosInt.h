b6dc0f8c4fd2807640172e25672e9dd99d0faf3c
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-17 19:22:45 +0200

    Update MinimizedPosInt.h

--- qt/windowmanager/win/MinimizedPosInt.h
+++ qt/windowmanager/win/MinimizedPosInt.h
@@ -17,7 +17,7 @@ public:
     void setValue(int newValue);
 
 private:
-    MinimizedPosInt() : sharedInt(std::make_shared<int>(50)) {}
+    MinimizedPosInt() : sharedInt(std::make_shared<int>(150)) {}
     std::shared_ptr<int> sharedInt;
     std::mutex mtx;
 
