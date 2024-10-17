5b48b3c3217ea341b5136c67ae5fe6d56f26813b
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-17 10:13:02 +0200

    Update MimizedPosInt.cpp

--- qt/windowmanager/win/MimizedPosInt.cpp
+++ qt/windowmanager/win/MimizedPosInt.cpp
@@ -1,28 +1,18 @@
-#include <iostream>
-#include <memory>
-#include <mutex>
-#include <stdexcept>
+#include "MinimizedPosInt.h"
 
-class MinimizedPosInt {
-public:
-    MinimizedPosInt() {
-        sharedInt = std::make_shared<int>(75);
+MinimizedPosInt::MinimizedPosInt() {
+    sharedInt = std::make_shared<int>(75);
+}
+
+int MinimizedPosInt::getValue() {
+    std::lock_guard<std::mutex> lock(mtx);
+    return *sharedInt;
+}
+
+void MinimizedPosInt::setValue(int newValue) {
+    std::lock_guard<std::mutex> lock(mtx);
+    if (newValue <= 0) {
+        throw std::invalid_argument("New value must be a positive integer.");
     }
-
-    int getValue() {
-        std::lock_guard<std::mutex> lock(mtx);
-        return *sharedInt;
-    }
-
-    void setValue(int newValue) {
-        std::lock_guard<std::mutex> lock(mtx);
-        if (newValue <= 0) {
-            throw std::invalid_argument("New value must be a positive integer.");
-        }
-        *sharedInt = newValue;
-    }
-
-private:
-    std::shared_ptr<int> sharedInt;
-    std::mutex mtx;
-};
+    *sharedInt = newValue;
+}
