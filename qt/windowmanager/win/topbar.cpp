170cd8097cf539044da2efcc970db6e2ced7906c
AcTh2 <68511655+acth2@users.noreply.github.com>
2024-10-17 09:15:07 +0200

    Update topbar.cpp

--- qt/windowmanager/win/topbar.cpp
+++ qt/windowmanager/win/topbar.cpp
@@ -202,7 +202,7 @@ void TopBar::minimizeWindow() {
         minusButton->hide();
         resizeButton->hide();
         trackedWindow->setGeometry(0, screenGeometry.height(), 0, 0);
-        this->setGeometry(0, screenGeometry.height() - 36, 25, 25);
+        this->setGeometry(0, screenGeometry.height() - 38, 25, 25);
         
         isMinimized = true;
     }
